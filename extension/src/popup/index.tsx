bd816850d055319b1155cc7a681009a97ea0e8cc
unknown <somiljaintkg1234@gmail.com>
2026-06-12 10:28:52 +0530

    mounts React into the popup HTML

---
+++ extension/src/popup/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Popup from './Popup'
+import './Popup.css'
+
+const root = document.getElementById('root')!
+ReactDOM.createRoot(root).render(
+  <React.StrictMode>
+    <Popup />
+  </React.StrictMode>
+)
